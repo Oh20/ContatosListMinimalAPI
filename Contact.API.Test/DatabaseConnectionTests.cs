defabeb6f122ea50829182cdbd4fe788361053e2
Oh20 <Oh20tony@outlook.com>
2024-07-27 14:31:52 +0000

    Corrigido falha no banco

--- Contact.API.Test/DatabaseConnectionTests.cs
+++ Contact.API.Test/DatabaseConnectionTests.cs
@@ -13,7 +13,7 @@ namespace ContatosListMinimalAPI.Tests
         public async Task TestDatabaseConnection()
         {
             var connectionString = "Server=tcp:testdbsql-001.database.windows.net,1433;Initial Catalog=Banco_lab_temp;Persist Security Info=False;User ID=azureadmin;Password=Senha@@5966;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            //var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+            //1var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
 
             if (string.IsNullOrEmpty(connectionString))
             {
